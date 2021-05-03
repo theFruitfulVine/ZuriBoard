e8aeee02dad07995ec03a0261b91ad46994ab39e
theFruitfulVine <47687864+theFruitfulVine@users.noreply.github.com>
2021-05-03 15:33:50 +0100

    Update 2.0

--- Ex_06_DjangoBlog/Ex_06_DjangoBlog/urls.py
+++ Ex_06_DjangoBlog/Ex_06_DjangoBlog/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fruitfulblog', include('fruitfulblog.urls'))
+    path('fruitfulblog', include('fruitfulblog.urls')),
 ]
--- Ex_06_DjangoBlog/fruitfulblog/tests.py
+++ Ex_06_DjangoBlog/fruitfulblog/tests.py
@@ -1,3 +1,43 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from django.urls import reverse
+
+from .models import Post
 
 # Create your tests here.
+class BlogTests(TestCase):
+    def setUp(self):
+        self.User = User.objects.create_user(
+            username='testuser',
+            email='test@gmail.com',
+            password='secret'
+        )
+
+        self.post = Post.objects.create(
+            title='A lovely title',
+            body='Lovely body',
+            author=self.User
+        )
+    
+    def test_string_representation(self):
+        post = Post(title='A lovely title')
+        self.assertEqual(str(post), post.title)
+
+    def test_post_content(self):
+        self.assertEqual(f'{self.post.title}', 'A lovely title')
+        self.assertEqual(f'{self.post.author}', 'testuser')
+        self.assertEqual(f'{self.post.body}', 'Lovely body')
+
+    def test_post_list_view(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Lovely body')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_post_detail_views(self):
+        response = self.client.get('/fruitfulblogpost/1/')
+        no_response = self.client.get('/fruitfulblogpost/100000/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, 'A lovely title')
+        self.assertTemplateUsed(response, 'post_detail.html')
