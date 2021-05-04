36e91ee499c0f1cd0f82d90ea4adc15f5cf8b851
theFruitfulVine <47687864+theFruitfulVine@users.noreply.github.com>
2021-05-04 23:31:23 +0100

    Update 3.0

--- Ex_06_DjangoBlog/Ex_06_DjangoBlog/urls.py
+++ Ex_06_DjangoBlog/Ex_06_DjangoBlog/urls.py
@@ -13,14 +13,21 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from endusers import views as enduser_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('fruitfulblog', include('fruitfulblog.urls')),
+    path('register/', enduser_views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='endusers/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name= 'endusers/logout.html'), name='logout'),
+    path('profile/', enduser_views.profile, name='profile'),
+    path('', include('fruitfulblog.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Ex_06_DjangoBlog/fruitfulblog/views.py
+++ Ex_06_DjangoBlog/fruitfulblog/views.py
@@ -1,28 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
 from .models import Post
 
 # Create your views here.
-class BlogListView(ListView):
-    model = Post
-    template_name = 'home.html'
+def homepage(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'fruitfulblog/home.html', context)
 
-class BlogDetailView(DetailView):
-    model = Post
-    template_name = 'post_detail.html'
-
-class BlogCreateView(CreateView):
-    model = Post
-    template_name = 'post_new.html'
-    fields = ['title', 'author', 'body']
-
-class BlogUpdateView(UpdateView):
-    model = Post
-    template_name = 'post_edit.html'
-    fields = ['title', 'body']
-
-class BlogDeleteView(DeleteView):
-    model = Post
-    template_name = 'post_delete.html'
-    success_url = reverse_lazy('home')
+def about(request):
+    return render(request, 'fruitfulblog/about.html', {'title': 'About'})
