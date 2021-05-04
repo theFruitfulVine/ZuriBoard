4b45932035252c0bd50bfbc3fdb0bfffc22fa64e
theFruitfulVine <47687864+theFruitfulVine@users.noreply.github.com>
2021-05-05 00:30:58 +0100

    BudgetApp 1.0

--- Ex_03_BudgetApp/KMDDavid_Ex03_BudgetApp_1.2.py
+++ Ex_03_BudgetApp/KMDDavid_Ex03_BudgetApp_1.2.py
@@ -1,17 +1,33 @@
 # This is a Budget app that helps you plan and stick with your budget
 
-database = {}
-
 class Budget:
 
     def __init__(self, section, amount):
-        self.currentBalance = 0
         self.section = section
         self.amount = amount
 
-    def transaction():
-        print ("**** Welcome to theKing's Budget App **** \n")
-        print ("What would you like to do? \n 1 - Create a new budget \n 2 - Deposit into an existing budget \n \3 - Withdraw from a budget \n 4 - Transfer between budgets \n 5 - Exit \n")
+    def deposit(self, amount):
+        self.amount = amount + amount
+        return "Amount of {} has been deposited into {} section".format(amount, self.section)
+    
+    def balance(self):
+        return "Current balance in {} section is: {}".format(self.section,self.amount)
 
+    def withdraw(self, amount, section):
+       self.amount = amount - amount
+       return "Amount of {} has been withdrawn from {} section".format(amount, self.section)
 
-Budget.transaction()
+    def transfer(self, amount, section):
+        self.amount = amount - amount
+        section.amount = amount + amount
+        return "Amount of {} was successfully transferred to {} section".format(amount, section.section)
+
+Offering = Budget('Offering', 4000)
+Food = Budget('Food',2000)
+Electricity = Budget('Electricity', 1000)
+Accommodation = Budget('Accommodation', 5000)
+
+print(Food.deposit(200))
+print(Accommodation.transfer(500, Food))
+print(Food.balance())
+print(Offering.withdraw(360, Offering))
