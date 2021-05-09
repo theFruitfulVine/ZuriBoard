9506919e2159fb4038d61d2664de1411e4143b9e
theFruitfulVine <47687864+theFruitfulVine@users.noreply.github.com>
2021-05-09 21:48:42 +0100

    Update 3.1

--- Ex_06_DjangoBlog/endusers/models.py
+++ Ex_06_DjangoBlog/endusers/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from PIL import Image
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -8,3 +9,13 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.enduser.username} Profile'
+
+    def save(self):
+        super().save() #this will automatically resize uploaded images
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
--- Ex_06_DjangoBlog/fruitfulblog/views.py
+++ Ex_06_DjangoBlog/fruitfulblog/views.py
@@ -1,4 +1,6 @@
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .models import Post
 
 # Create your views here.
@@ -7,6 +9,50 @@ def homepage(request):
         'posts': Post.objects.all()
     }
     return render(request, 'fruitfulblog/home.html', context)
+
+class PostListView(ListView): #class-based view
+    model = Post
+    template_name = 'fruitfulblog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+class PostDetailView(DetailView): #class-based view
+    model = Post
+
+class PostCreateView(LoginRequiredMixin, CreateView): #class-based view
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView): #allow post updates
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self): #prevent updating/editing another user's post
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView): #delete posts
+    model = Post
+    success_url = '/'
+    
+    def test_func(self): #prevent updating/editing another user's post
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else:
+            return False
+
 
 def about(request):
     return render(request, 'fruitfulblog/about.html', {'title': 'About'})
