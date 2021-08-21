e8b5fa7d03dbdff4ae7fc54f2c5f19fa6af0f6de
theFruitfulVine <47687864+theFruitfulVine@users.noreply.github.com>
2021-08-21 16:27:39 +0100

    Updated settings.py
    Set Debug = False
    Removed Secret key

--- Ex_06_DjangoBlog/Ex_06_DjangoBlog/settings.py
+++ Ex_06_DjangoBlog/Ex_06_DjangoBlog/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5v1s1i@!q!v&(^1)q79z^97ld@g+m#da+i(cg)bkv9+9kk)d4('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
